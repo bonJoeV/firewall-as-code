0a26966c93f713009750280b57b4e8199e498c06
Vincent Misson <vincent.misson@michelin.com>
2021-08-31 01:15:34 +0200

    Update gitlab-ci to work on main

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "provider_subscription_id" {
-  description = "Subscription ID"
-  type        = string
-  sensitive   = true
-}
-
 variable "firewall_policy_resource_group" {
   description = "RG of the Firewall Policy"
   type        = string
